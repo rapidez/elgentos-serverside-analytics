f7e2b841d731265d5094b2c83d2ea1e1f59afe00
Roy Duineveld <royduineveld@gmail.com>
2021-12-17 15:37:41 +0100

    Only send the user id when there is one set

--- resources/js/analytics.js
+++ resources/js/analytics.js
@@ -1,28 +1,30 @@
 document.addEventListener('turbolinks:load', () => {
     window.app.$on('checkout-credentials-saved', (data) => {
-        let options = { headers: {} }
-        if (localStorage.token) {
-            options['headers']['Authorization'] = `Bearer ${localStorage.token}`
-        }
-
-        axios.post(config.magento_url + '/graphql', {
-            query:
-            `mutation StartTransaction(
-                $cartId: String
-                $gaUserId: String
-            ) {
-                AddGaUserId (
-                    cartId: $cartId
-                    gaUserId: $gaUserId
-                ) {
-                    cartId
-                    maskedId
-                }
-            }`,
-            variables: {
-                cartId: localStorage.mask,
-                gaUserId: window.config.gaUserId
+        if (window.config.gaUserId) {
+            let options = { headers: {} }
+            if (localStorage.token) {
+                options['headers']['Authorization'] = `Bearer ${localStorage.token}`
             }
-        }, options)
+
+            axios.post(config.magento_url + '/graphql', {
+                query:
+                `mutation StartTransaction(
+                    $cartId: String
+                    $gaUserId: String
+                ) {
+                    AddGaUserId (
+                        cartId: $cartId
+                        gaUserId: $gaUserId
+                    ) {
+                        cartId
+                        maskedId
+                    }
+                }`,
+                variables: {
+                    cartId: localStorage.mask,
+                    gaUserId: window.config.gaUserId
+                }
+            }, options)
+        }
     });
 })
